5ca7fcc0fecf946200ccdac57ae56b58fe95a17f
Luuk Minten <290036@studeny.fontys.nl>
2016-03-17 10:30:01 +0100

    Woeps

--- Test game/UnitTestProject1/UnitTest1.cs
+++ Test game/UnitTestProject1/UnitTest1.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test_game;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class TestGameTest1
     {
+        private LetsPlayAGame game;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            game = new LetsPlayAGame();
+        }
+
         [TestMethod]
         public void TestIfTrue()
         {
             Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestIfOne()
+        {
+            Assert.AreEqual(1, game.RollD6());
         }
     }
 }
