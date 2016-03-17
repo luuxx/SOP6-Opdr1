50a9a4c982ebc828ae62e2d5718975fac814acba
Luuk Minten <290036@studeny.fontys.nl>
2016-03-17 10:30:28 +0100

    New class

---
+++ Test game/Test game/LetsPlayAGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_game
+{
+    public class LetsPlayAGame
+    {
+        public int RollD6()
+        {
+            return 1;
+        }
+    }
+}
